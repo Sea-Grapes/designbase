59161738eeeccd6513667ae2bfe352f721dc926d
Sea-Grapes <natuloid@gmail.com>
2026-08-09 22:12:18 -0700

    save

--- app/main.ts
+++ app/main.ts
@@ -9,19 +9,40 @@ export async function pickFolder() {
 const DB_NAME = 'designbase'
 const STORE_NAME = 'fs'
 
-function openDb() {
-    return new Promise((res, rej) => {
+function openDb(): Promise<IDBDatabase> {
+    return new Promise((ok, err) => {
         const req = indexedDB.open(DB_NAME, 1)
         req.onupgradeneeded = () => {
             req.result.createObjectStore(STORE_NAME)
         }
-        req.onsuccess = () => res(req.result)
-        req.onerror = () => rej(req.error)
+        req.onsuccess = () => ok(req.result)
+        req.onerror = () => err(req.error)
     })
 }
 
 const db = await openDb()
 
-async function set(key, value) {
-    
+function set(key, value): Promise<void> {
+    return new Promise((ok, err) => {
+        const t = db.transaction(STORE_NAME, 'readwrite')
+        t.objectStore(STORE_NAME).put(value, key)
+        t.oncomplete = () => ok()
+        t.onerror = () => err(t.error)
+    })
+}
+
+function get(key: IDBValidKey): Promise<unknown> {
+    return new Promise((ok, err) => {
+        const t = db.transaction(STORE_NAME, 'readonly')
+        const req = t.objectStore(STORE_NAME).get(key)
+        req.onsuccess = () => ok(req.result)
+        req.onerror = () => err(req.error)
+    })
+}
+
+function del(key: IDBValidKey): Promise<void> {
+    return new Promise((ok, err) => {
+        const t = db.transaction(STORE_NAME, 'readwrite')
+        
+    })
 }
