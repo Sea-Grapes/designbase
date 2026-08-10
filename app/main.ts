13faaeeac622c338174d2d6ff0ca9fc6ebc1a0dc
Sea-Grapes <natuloid@gmail.com>
2026-08-09 21:17:29 -0700

    save

--- app/main.ts
+++ app/main.ts
@@ -1,7 +1,27 @@
 declare function showDirectoryPicker(): Promise<FileSystemDirectoryHandle>
 
 
-async function pickFolder() {
+export async function pickFolder() {
     if (!('showDirectoryPicker' in window)) return console.error('No file system access')
     const handle = await showDirectoryPicker()
+}
+
+const DB_NAME = 'designbase'
+const STORE_NAME = 'fs'
+
+function openDb() {
+    return new Promise((res, rej) => {
+        const req = indexedDB.open(DB_NAME, 1)
+        req.onupgradeneeded = () => {
+            req.result.createObjectStore(STORE_NAME)
+        }
+        req.onsuccess = () => res(req.result)
+        req.onerror = () => rej(req.error)
+    })
+}
+
+const db = await openDb()
+
+async function set(key, value) {
+    
 }
